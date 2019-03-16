c1f8067af24d3e8c5f3b23848589490d7aa56db5
loc huynh <lochp.hcmc@gmail.com>
2019-03-17 02:17:14 +0700

    fix

---
+++ nodejs/myfirst.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var dt = require('./myfirstmodule');
+var url = require('url');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Date function' + dt.myDateTime());
+    res.end();
+}).listen(8080);
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + ' ' + q.month;
+    res.end(txt);
+}).listen(8081);
